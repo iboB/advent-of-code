3cdeb4a4e7337a5cbab6ba4a0125519396758db9
Borislav Stanimirov <b.stanimirov@abv.bg>
2020-12-15 10:16:53 +0200

    Cleanup

--- 15/day15.rb
+++ 15/day15.rb
@@ -1,19 +1,12 @@
-
-ar = [0,1,4,13,15,12,16]
-ind = ar.each_with_index.to_a.to_h
+ind = [0,1,4,13,15,12,16].each_with_index.to_a.to_h
 len = ind.length
 nn = 0
 
 N = 30_000_000
 (N-len-1).times do |x|
   i = ind[nn]
-  if !i
-    ind[nn] = len
-    nn = 0
-  else
-    ind[nn] = len
-    nn = len - i
-  end
+  ind[nn] = len
+  nn = i ? len - i : 0
   len += 1
 end
 
