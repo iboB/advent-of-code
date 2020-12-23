dea6225206ab542b170b33f812d7993755327424
Borislav Stanimirov <b.stanimirov@abv.bg>
2020-12-23 10:13:55 +0200

    Rename card to cup

--- 23/day23.cpp
+++ 23/day23.cpp
@@ -13,65 +13,65 @@ static constexpr std::string_view input =
     // "389125467"; // example
 
 using list = std::list<int>;
-list cards;
-std::vector<std::list<int>::iterator> card_ids;
+list cups;
+std::vector<std::list<int>::iterator> cup_ids;
 
-void push_card(int card) {
-    cards.push_back(card);
-    card_ids[size_t(card) - 1] = std::next(cards.end(), -1);
+void push_cup(int cup) {
+    cups.push_back(cup);
+    cup_ids[size_t(cup) - 1] = std::next(cups.end(), -1);
 }
 
 void parse_input() {
-    card_ids.clear();
-    cards.clear();
-    card_ids.resize(input.length());
+    cup_ids.clear();
+    cups.clear();
+    cup_ids.resize(input.length());
     for (auto c : input) {
-        int card = c - '0';
-        assert(card > 0);
-        push_card(card);
+        int cup = c - '0';
+        assert(cup > 0);
+        push_cup(cup);
     }
 }
 
 void add_1mil() {
     constexpr size_t N = 1'000'000;
-    card_ids.resize(N);
-    int card = int(cards.size());
-    while (cards.size() != N) {
-        push_card(++card);
+    cup_ids.resize(N);
+    int cup = int(cups.size());
+    while (cups.size() != N) {
+        push_cup(++cup);
     }
 }
 
 void play_move() {
     // move cur to back
     list helper;
-    helper.splice(helper.begin(), cards, cards.begin());
-    cards.splice(cards.end(), helper);
+    helper.splice(helper.begin(), cups, cups.begin());
+    cups.splice(cups.end(), helper);
 
     // take three
-    helper.splice(helper.begin(), cards, cards.begin(), std::next(cards.begin(), 3));
+    helper.splice(helper.begin(), cups, cups.begin(), std::next(cups.begin(), 3));
 
     std::array<int, 3> taken;
     std::copy(helper.begin(), helper.end(), taken.begin());
 
     // find where to put them
-    int next_card = cards.back();
+    int next_cup = cups.back();
     do {
-        --next_card;
-        if (next_card == 0) next_card = int(card_ids.size());
-    } while (std::find(taken.begin(), taken.end(), next_card) != taken.end());
+        --next_cup;
+        if (next_cup == 0) next_cup = int(cup_ids.size());
+    } while (std::find(taken.begin(), taken.end(), next_cup) != taken.end());
 
-    auto next_card_pos = card_ids[next_card-1];
-    ++next_card_pos;
-    cards.splice(next_card_pos, helper);
+    auto next_cup_pos = cup_ids[next_cup-1];
+    ++next_cup_pos;
+    cups.splice(next_cup_pos, helper);
 }
 
 // reorder them to start after '1' and remove '1'
-void normalize_cards() {
+void normalize_cups() {
     list helper;
-    auto one = card_ids[0];
-    helper.splice(helper.begin(), cards, cards.begin(), one);
-    cards.splice(cards.end(), helper);
-    cards.pop_front();
+    auto one = cup_ids[0];
+    helper.splice(helper.begin(), cups, cups.begin(), one);
+    cups.splice(cups.end(), helper);
+    cups.pop_front();
 }
 
 int main() {
@@ -81,8 +81,8 @@ int main() {
     {
         parse_input();
         for (int i = 0; i < 100; ++i) play_move();
-        normalize_cards();
-        for (auto c : cards) std::cout << c;
+        normalize_cups();
+        for (auto c : cups) std::cout << c;
         std::cout << '\n';
     }
 
@@ -91,8 +91,8 @@ int main() {
         parse_input();
         add_1mil();
         for (int i = 0; i < 10'000'000; ++i) play_move();
-        normalize_cards();
-        auto b = cards.begin();
+        normalize_cups();
+        auto b = cups.begin();
         std::cout << uint64_t(*b) * uint64_t(*++b) << '\n';
     }
 
