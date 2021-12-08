165f7cd8c16d7570f1e55668f8327e9ec8fd27cf
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-12-08 10:52:27 +0200

    2021 Day 8 b

--- 2021/08/day08b.rb
+++ 2021/08/day08b.rb
@@ -1,5 +1,22 @@
-input = File.readlines('input.txt').map { |l|
-  l.strip.split(' | ').map(&:split)
+PerDigit = %w(abcefg cf acdeg acdfg bcdf abdfg abdefg acf abcdefg abcdfg)
+Eight = PerDigit[8].split(//)
+Combos = PerDigit.map { |s| s.split(//).map { _1.ord - 'a'.ord } }
+
+Perms = (0...7).to_a.permutation.map { |perm|
+  Combos.map { |c| c.map { |i| Eight[perm[i]] }.sort.join }
 }
 
-p input
+def solve(keys, target)
+  keys += target # target can be useful too as it also contains valid entries
+  keys.map! { _1.split(//).sort.join }.uniq!
+
+  found = Perms.detect { (keys - _1).empty? }
+
+  raise 'uh-oh' if !found
+
+  target.map { found.index _1.split(//).sort.join }.join.to_i
+end
+
+p File.readlines('input.txt').map { |l|
+  solve(*l.strip.split(' | ').map(&:split))
+}.sum
