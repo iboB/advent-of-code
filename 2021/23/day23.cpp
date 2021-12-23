733cf4c7cf96fe437c5e7b47019903c8e40e4b0c
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-12-23 12:26:10 +0200

    2021 Day 23 b

--- 2021/23/day23.cpp
+++ 2021/23/day23.cpp
@@ -1,3 +1,4 @@
+// brute force recurse all possible games
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -18,13 +19,14 @@ struct Map {
         bool done() const { return moves > 1; }
         void clear() { type = E; }
     };
+    static constexpr int RD = 4;
     struct Cell {
-        Amphi occupants[2];
-        bool empty() const {
-            return !occupants[0] && !occupants[1];
-        }
+        Amphi occupants[RD];
         bool done() const {
-            return occupants[0] && occupants[0].done() && occupants[1] && occupants[1].done();
+            for (auto& o : occupants) {
+                if (!o || !o.done()) return false;
+            }
+            return true;
         }
         Amphi& occupant(int i = 0) { return occupants[i]; }
         const Amphi& occupant(int i = 0) const { return occupants[i]; }
@@ -61,6 +63,22 @@ struct Map {
         return {begin, end};
     }
 
+    void markDone() {
+        // mark ones already in place as dones
+        for (int i = 0; i < 4; ++i) {
+            auto r = Map::rooms[i];
+            for (int oi = RD-1; oi >= 0; --oi) {
+                auto& o = cells[r].occupants[oi];
+                if (o.type == i) {
+                    o.moves = 100;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+    }
+
     void print() const {
         static constexpr char T2N[] = { 'A', 'B', 'C', 'D', '.' };
         for (int i = 0; i < NC; ++i) {
@@ -72,7 +90,7 @@ struct Map {
             }
         }
         cout << '\n';
-        for (int r = 0; r < 2; ++r) {
+        for (int r = 0; r < Map::RD; ++r) {
             for (int i = 0; i < NC; ++i) {
                 if (room(i)) {
                     cout << T2N[cells[i].occupant(r).type];
@@ -90,26 +108,12 @@ Map readInput() {
     ifstream fin(INPUT_PATH"/input.txt");
 
     Map ret;
-    int i = 0;
-    while (true) {
+    for(int i=0; i<4; ++i) {
         string row;
         getline(fin, row);
         if (i > 1) {
             for (auto r : Map::rooms) {
                 ret.cells[r].occupants[i - 2].type = Map::AType(row[r+1] - 'A');
-            }
-        }
-        ++i;
-        if (i == 4) break;
-    }
-
-    // mark ones already in place as dones
-    for (int i = 0; i < 4; ++i) {
-        auto r = Map::rooms[i];
-        if (ret.cells[r].occupant(1).type == i) {
-            ret.cells[r].occupant(1).moves = 100;
-            if (ret.cells[r].occupant(0).type == i) {
-                ret.cells[r].occupant(0).moves = 100;
             }
         }
     }
@@ -120,11 +124,11 @@ Map readInput() {
 int minCost = INT_MAX;
 
 void bf(const Map& map, const int total) {
+    if (total >= minCost) return;
     if (map.done()) {
         if (minCost > total) minCost = total;
         return;
     }
-    if (total > minCost) return;
 
     auto recurse = [&](int from, int levelfrom, int to, int levelto) {
         auto& o = map.cells[from].occupants[levelfrom];
@@ -134,52 +138,89 @@ void bf(const Map& map, const int total) {
         newMap.cells[to].occupants[levelto] = {o.type, uint8_t(o.moves + 1)};
         auto cost = (abs(from - to) + levelfrom + levelto + 1) * Map::costPerMove[o.type];
         bf(newMap, total + cost);
-        //printf("%d %d -> %d %d (%d)\n", from, levelfrom, to, levelto, cost);
-        //newMap.print();
-        //cout << endl;
     };
 
     for (int pos = 0; pos < Map::NC; ++pos) {
         auto& src = map.cells[pos];
-        if (auto& o = src.occupant()) {
-            if (o.moves == 0) { // never moved (it's a room)
-                auto [b, e] = map.hallLimits(pos);
-                for (int hi = b; hi < e; ++hi) {
-                    if (Map::room(hi)) continue;
-                    recurse(pos, 0, hi, 0);
-                }
+        if (Map::room(pos)) {
+            int level = 0;
+            for (; level < Map::RD; ++level) {
+                if (src.occupants[level]) break;
             }
-            else if (o.moves == 1) { // moved once (in the hallway)
-                auto targetPos = Map::rooms[o.type];
-                auto [b, e] = map.hallLimits(pos);
-                if (targetPos < b || targetPos >= e) continue; // can't go home
-                auto& target = map.cells[targetPos];
-                if (target.occupant()) continue; // home is blocked
-                if (target.occupant(1)) {
-                    if (target.occupant(1).type != o.type) continue; // home bottom is occupied by someone who needs to move
-                    recurse(pos, 0, targetPos, 0);
-                }
-                else {
-                    recurse(pos, 0, targetPos, 1);
-                }
+            if (level == Map::RD) continue; // empty room
+            auto& o = src.occupants[level];
+            if (o.done()) continue; // room done (so far)
+            auto [b, e] = map.hallLimits(pos);
+            for (int hi = b; hi < e; ++hi) {
+                if (Map::room(hi)) continue;
+                recurse(pos, level, hi, 0);
             }
-            // otherwise occupant is done
         }
-        else if (auto& o = src.occupant(1)) { // bottom of room
-            if (o.moves == 0) { // never moved (it's a room)
-                auto [b, e] = map.hallLimits(pos);
-                for (int hi = b; hi < e; ++hi) {
-                    if (Map::room(hi)) continue;
-                    recurse(pos, 1, hi, 0);
+        else {
+            auto& o = src.occupant();
+            if (!o) continue;
+            assert(o.moves == 1);
+            auto targetPos = Map::rooms[o.type];
+            auto [b, e] = map.hallLimits(pos);
+            if (targetPos < b || targetPos >= e) continue; // road to home is blocked
+            auto& target = map.cells[targetPos];
+            auto targetLevel = Map::RD;
+            for (int i = Map::RD - 1; i >= 0; --i) {
+                auto& roomo = target.occupants[i];
+                if (!roomo) {
+                    targetLevel = i;
+                    break;
+                }
+                else if (roomo.type != o.type) {
+                    break;
                 }
             }
+            if (targetLevel == Map::RD) continue; // home bottom is occupied by someone who needs to move
+            recurse(pos, 0, targetPos, targetLevel);
         }
     }
 }
 
 int main() {
-    timer t;
     auto map = readInput();
-    bf(map, 0);
-    cout << minCost << endl;
+
+    // a
+    {
+        timer t;
+        auto mapA = map;
+        for (auto r : Map::rooms) {
+            for (int i = 2; i < Map::RD; ++i) {
+                mapA.cells[r].occupants[i].type = Map::AType(r / 2 - 1);
+            }
+        }
+        // mapA.print();
+        mapA.markDone();
+        bf(mapA, 0);
+        cout << "a: " << minCost << endl;
+    }
+
+    minCost = INT_MAX;
+
+    // b
+    {
+        timer t;
+        auto mapB = map;
+        static_assert(Map::RD == 4);
+        for (auto r : Map::rooms) {
+            auto& c = mapB.cells[r];
+            c.occupants[3] = c.occupants[1];
+        }
+        mapB.cells[Map::rooms[0]].occupants[1].type = Map::D;
+        mapB.cells[Map::rooms[0]].occupants[2].type = Map::D;
+        mapB.cells[Map::rooms[1]].occupants[1].type = Map::C;
+        mapB.cells[Map::rooms[1]].occupants[2].type = Map::B;
+        mapB.cells[Map::rooms[2]].occupants[1].type = Map::B;
+        mapB.cells[Map::rooms[2]].occupants[2].type = Map::A;
+        mapB.cells[Map::rooms[3]].occupants[1].type = Map::A;
+        mapB.cells[Map::rooms[3]].occupants[2].type = Map::C;
+
+        mapB.markDone();
+        bf(mapB, 0);
+        cout << "b: " << minCost << endl;
+    }
 }
