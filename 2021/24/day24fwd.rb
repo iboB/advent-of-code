dbbd9276c4ec61115c567b3d4ef462031217dd38
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-12-25 06:56:43 +0200

    Day 24 WiP

--- 2021/24/day24fwd.rb
+++ 2021/24/day24fwd.rb
@@ -2,83 +2,92 @@ class FalseClass; def to_i; 0 end end
 class TrueClass; def to_i; 1 end end
 
 class Val
-  def initialize(r)
+  def initialize(r, c)
     @range = r
+    @str = c
   end
 
   attr :range, :str
 
+  def op_parens(a, op, b)
+    "(#{a} #{op} #{b})"
+  end
+
+  def op(a, op, b)
+    "#{a} #{op} #{b}"
+  end
+
   def addi(i)
     return self if i == 0
-    Val.new @range.map { _1 + i }
+    Val.new @range.map { _1 + i }, op_parens(@str, '+', i)
   end
 
   alias_method :iadd, :addi
 
   def addv(v)
-    Val.new @range.product(v.range).map { _1 + _2 }.uniq
+    Val.new @range.product(v.range).map { _1 + _2 }.uniq, op_parens(@str, '+', v.str)
   end
 
   def muli(i)
     return self if i == 1
     return 0 if i == 0
-    Val.new @range.map { _1 * i }
+    Val.new @range.map { _1 * i }, op(@str, '*', i)
   end
 
   alias_method :imul, :muli
 
   def mulv(v)
-    Val.new @range.product(v.range).map { _1 * _2 }.uniq
+    Val.new @range.product(v.range).map { _1 * _2 }.uniq, op(@str, '*', v.str)
   end
 
   def divi(i)
     return self if i == 1
     nr = @range.map { _1 / i }.uniq
     return nr[0] if nr.length == 1
-    Val.new nr
+    Val.new nr, op(@str, '/', i)
   end
 
   def idiv(i)
     return 0 if i == 0
     nr = @range.select { _1 != 0 }.map { i / _1 }.uniq
     return nr[0] if nr.length == 1
-    Val.new nr
+    Val.new nr, op(i, '/', @str)
   end
 
   def divv(v)
     @range.product(v.range.select { _1 != 0 }).map { _1 / _2 }.uniq
     return nr[0] if nr.length == 1
-    Val.new nr
+    Val.new nr, op(@str, '/', v.str)
   end
 
   def modi(i)
     nr = @range.select { _1 >= 0 }.map { _1 % i }.uniq
     return nr[0] if nr.length == 1
-    Val.new nr
+    Val.new nr, op_parens(@str, '%', i)
   end
 
   def imod(i)
     nr = @range.select { _1 > 0 }.map { i % _1 }.uniq
     return nr[0] if nr.length == 1
-    Val.new nr
+    Val.new nr, op_parens(i, '%', @str)
   end
 
   def modv(v)
     @range.select { _1 >= 0 }.product(v.range.select { _1 > 0 }).map { _1 % _2 }.uniq
     return nr[0] if nr.length == 1
-    Val.new nr
+    Val.new nr, op_parens(@str, '%', v.str)
   end
 
   def eqli(i)
     return 0 if !@range.include?(i)
-    Val.new [0, 1]
+    Val.new [0, 1], op_parens(@str, '==', i) + ".to_i"
   end
 
   alias_method :ieql, :eqli
 
   def eqlv(v)
     return 0 if (@range & v.range).empty?
-    Val.new [0, 1]
+    Val.new [0, 1], op_parens(@str, '==', v.str) + ".to_i"
   end
 
   def perform(op, arg)
@@ -110,7 +119,7 @@ input = File.readlines('input.txt').map { |l|
   a = a.ord - ?w.ord
 
   if op == 'inp'
-    regs[a] = Val.new (1..9).to_a
+    regs[a] = Val.new (1..9).to_a, "m[#{ii}]"
     ii += 1
   else
     va = regs[a]
@@ -127,4 +136,6 @@ z = regs[-1]
 puts z.range.length
 puts z.range.min
 puts z.range.max
+puts z.str
+
 
