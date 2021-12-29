8fe178cf91ba11eb22e7a81dc65f2452e09f268a
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-12-29 07:33:18 +0200

    2015 Day 6

--- 2015/06/day06.rb
+++ 2015/06/day06.rb
@@ -1,18 +1,3 @@
-class Range
-  def &(other)
-    return nil if (self.max < other.begin or other.max < self.begin)
-    [self.begin, other.begin].max..[self.max, other.max].min
-  end
-  def -(other)
-    i = self & other
-    return [self] if !i
-    ret = []
-    ret << (self.min..other.min-1) if other.min > self.min
-    ret << (other.max+1..self.max) if other.max < self.max
-    ret
-  end
-end
-
 class Box
   def initialize(min = nil, max = nil)
     @min = min
@@ -27,8 +12,7 @@ class Box
   def &(other)
     rmin = @min.zip(other.min).map(&:max)
     rmax = @max.zip(other.max).map(&:min)
-
-    return nil if @min.zip(@max).any? { _1 > _2 }
+    return nil if rmin.zip(rmax).any? { _1 > _2 }
     Box.new(rmin, rmax)
   end
   def -(other)
@@ -60,22 +44,50 @@ class Box
 
     ret
   end
+  def subtract_multi(others)
+    ret = [self]
+    others.each do |o|
+      ret.map! { _1 - o }.flatten!
+    end
+    ret
+  end
   def volume
     @min.zip(@max).map { _2 - _1 + 1 }.inject(&:*)
   end
 end
 
-level = [Box.new([0, 0], [999, 999])]
-on = []
-
-File.readlines('input.txt').map { |l|
+input = File.readlines('input.txt').map { |l|
   m = l.strip.match /(\w+) (\d+),(\d+) through (\d+),(\d+)/
   [m[1], Box.new([m[2].to_i, m[3].to_i], [m[4].to_i, m[5].to_i])]
-}.reverse.each { |op, b|
-  on += level.map { _1 & b }.compact if op == 'on'
-
-  if op == 'toggle'
-  else
-  end
 }
 
+# a
+on = []
+input.each do |op, box|
+  new_ons = []
+  new_ons = box.subtract_multi(on) if op == 'toggle'
+  on.map! { _1 - box }.flatten!
+  new_ons = [box] if op == 'on'
+  on += new_ons
+end
+p on.map(&:volume).sum
+
+# b
+add = []
+input.each do |op, box|
+  if op == 'on'
+    add << box
+  elsif op == 'toggle'
+    add += [box] * 2
+  else
+    r = [box]
+    add.map! { |a|
+      next a if !(a & box)
+      diff = a.subtract_multi(r)
+      r.map! { _1 - a }.flatten!
+      diff
+    }.flatten!
+  end
+end
+
+p add.map(&:volume).sum
