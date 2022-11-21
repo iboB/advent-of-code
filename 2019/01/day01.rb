0bf95df51089087ddcdd91725b5989e8aa4ba52f
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-11-21 05:28:58 +0200

    Mark a and b

--- 2019/01/day01.rb
+++ 2019/01/day01.rb
@@ -3,10 +3,13 @@ input = File.readlines('input.txt').map { |l| l.to_i }
 f = ->() { input.map! { |i| i/3 - 2 } }
 
 sum = f.().sum
+
+# a
 p sum
 
 while !input.empty?
   sum += (f.().select!(&:positive?) || input).sum
 end
 
+# b
 p sum
--- 2019/04/day04.rb
+++ 2019/04/day04.rb
@@ -3,7 +3,11 @@ a = 236491.upto(713787).select {
 }.select {
   |n| n.to_s.split(//).each_cons(2).any? { |a, b| a==b }
 }
+
+# a
 p a.length
+
+# b
 p a.select {
   |n| n.to_s.split(//).chunk(&:itself).any? { |c| c[1].length == 2 }
 }.length
