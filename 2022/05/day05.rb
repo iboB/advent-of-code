47d5e397703742908dbb59793a0fceb64d8c1497
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-12-05 13:23:16 +0200

    2022 Day 5

---
+++ 2022/05/day05.rb
@@ -0,0 +1,24 @@
+icrates, moves = File.read('input.txt').split("\n\n")
+
+crates_a = icrates.lines.reverse.drop(1).map { |line|
+  1.step(line.length, 4).map { line[_1] }
+}.transpose.map { |crate|
+  crate.filter { _1 != ' '}
+}
+
+crates_b = Marshal.load Marshal.dump crates_a
+
+moves.lines.each do |l|
+  n, from, to = l.scan(/\d+/).map(&:to_i)
+  from -= 1
+  to -= 1
+
+  n.times do
+    crates_a[to] << crates_a[from].pop
+  end
+
+  crates_b[to] += crates_b[from].pop(n)
+end
+
+p crates_a.map(&:last).join
+p crates_b.map(&:last).join
