4a2440aaae97dfa11e033da7ada77f3063e75600
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-12-09 07:57:21 +0200

    Simplify 2022 Day 9

--- 2022/09/day09.rb
+++ 2022/09/day09.rb
@@ -1,44 +1,35 @@
 require 'matrix'
 
-Dirs = {
-  ?R => Vector[1, 0],
-  ?L => Vector[-1, 0],
-  ?U => Vector[0, 1],
-  ?D => Vector[0, -1],
-}
+snake = Array.new(10) { Vector[0, 0] }
+tpos_a = {}
+tpos_b = {}
 
-def solve(commands, snake_len)
-  snake = Array.new(snake_len) { Vector[0, 0] }
-  tpos = {}
-
-  commands.each do |cmd|
-    # p cmd
-    cmd[1].times {
-      snake[0] += Dirs[cmd[0]]
-      snake.each_cons(2) { |h, t|
-        dir = h-t
-        if (dir.any? { _1.abs > 1 })
-          # hacky preserve t
-          t[0..1] = t + dir.map { _1 == 0 ? 0 : _1/_1.abs}
-        else
-          break
-        end
-      }
-      tpos[snake.last.to_a.join(';')] = true
-      # p snake
-    }
+File.readlines('input.txt').each { |l|
+  # p l
+  dir, moves = l.split(' ')
+  dir = case dir
+  when ?R then Vector[1, 0]
+  when ?L then Vector[-1, 0]
+  when ?U then Vector[0, 1]
+  when ?D then Vector[0, -1]
   end
-  tpos.length
-end
-
-input = File.readlines('input.txt').map { |l|
-  d, s = l.split(' ')
-  [d, s.to_i]
+  moves.to_i.times {
+    snake[0] += dir
+    snake.each_cons(2) { |h, t|
+      delta = h-t
+      if (delta.any? { _1.abs > 1 })
+        # hacky preserve t
+        t[0..1] = t + delta.map { _1 == 0 ? 0 : _1/_1.abs}
+      else
+        break
+      end
+    }
+    tpos_a[snake[1].to_s] = true
+    tpos_b[snake.last.to_s] = true
+    # p snake
+  }
 }
 
-# a
-p solve(input, 2)
-
-# b
-p solve(input, 10)
+p tpos_a.length
+p tpos_b.length
 
