7082976fecf6a1215425214a1ea0cbb04a987663
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-12-19 07:39:13 +0200

    2022 Day 19 read input

---
+++ 2022/19/day19.rb
@@ -0,0 +1,20 @@
+def rsolve(min, prices, robots, resources, &block)
+
+  robots.each_with_index { reources[_2] += 1 } # mine
+end
+
+def solve(prices)
+  rob = 1
+  ore = 0
+  24.times.map {
+
+  }
+end
+
+p File.read('input.txt').strip.split("\n").join.split("Blueprint")[1..1].map { |l|
+  solve(l.split(':')[1].scan(/\d+/).map(&:to_i))
+}
+
+
+
+
