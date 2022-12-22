592c4fd794b40f595894c5dd47f1db2c39cc81e9
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-12-22 09:09:04 +0200

    2022 22 part 1

--- 2022/22/day22.rb
+++ 2022/22/day22.rb
@@ -1,24 +1,23 @@
 require 'matrix'
 map, path = File.read('input.txt').split("\n\n")
 
-map.gsub!("\n", ' ')
-map = map.lines.map { _1.split(//) }
-map[-1] << ' '
+map = map.lines.map { _1.gsub("\n", ' ').split(//) + [' '] }
+w = map[0].size
+map.each { _1.fill(' ', _1.size...w) }
 map << [' '] * map[0].size
-
-pos = Vector[map[0].index('.'), 0]
-dir = 0
-Dirs = [Vector[1, 0], Vector[0, 1], Vector[-1, 0], Vector[0, -1]]
-Turns = {'R' => 1, 'L' => -1}
 
 path = path.strip.split(/(?=[R|L])/).map { _1.split(/(?<=[R|L])/) }.flatten.map { |i|
   next i if i == ?R || i == ?L
   i.to_i
 }
 
+pos = Vector[map[0].index('.'), 0]
+dir = 0
+Dirs = [Vector[1, 0], Vector[0, 1], Vector[-1, 0], Vector[0, -1]]
+Turns = {'R' => 1, 'L' => -1}
+
 # path.each
-3.times do |i|
-  p i
+path.each do |i|
   if String === i
     dir += Turns[i]
     dir %= 4
@@ -36,5 +35,5 @@ path = path.strip.split(/(?=[R|L])/).map { _1.split(/(?<=[R|L])/) }.flatten.map 
   end
 end
 
-p pos
-p dir
+x, y = pos.to_a.map { _1 + 1 }
+p y*1000 + x*4 + dir
