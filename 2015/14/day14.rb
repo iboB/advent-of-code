4840847a17a8f9a8e3e1ca40540279982275fb57
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-12-23 09:45:35 +0200

    2015 14

--- 2015/14/day14.rb
+++ 2015/14/day14.rb
@@ -1,4 +1,26 @@
-deer = File.readlines('input.txt').map { |l|
-  s = l.split(' ')
-  [s[0], s[3].to_i, s[6].to_i, s[-2].to_i]
-}
+require 'matrix'
+
+Secs = 2503
+persec = File.readlines('input.txt').map { |l|
+  speed, tmove, trest = l.scan(/\d+/).map(&:to_i)
+  period = tmove + trest
+  dpp = speed * tmove
+
+  1.upto(Secs).map { |sec|
+    div, mod = sec.divmod(period)
+    dist = div * dpp
+    mod = tmove if mod > tmove
+    dist + speed * mod
+  }
+}.transpose
+
+# part 1
+p persec[-1].max
+
+# part 2
+p persec.map { |round|
+  Vector[*round.map { _1 / round.max }]
+}.inject(:+).to_a.max
+
+
+
