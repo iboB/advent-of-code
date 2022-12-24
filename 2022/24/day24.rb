0085f9e351d83000294b151c331db7df79959696
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-12-24 13:54:38 +0200

    2022 24

--- 2022/24/day24.rb
+++ 2022/24/day24.rb
@@ -1,5 +1,6 @@
 require 'matrix'
 
+Dirs = [Vector[1, 0], Vector[-1, 0], Vector[0, 1], Vector[0, -1]]
 DC = {'>' => Vector[1, 0], '<' => Vector[-1, 0], 'v' => Vector[0, 1], '^' => Vector[0, -1]}
 
 w = 0
@@ -15,34 +16,60 @@ Bliz = File.readlines('input.txt')[1..-2].map.with_index { |l, y|
 W = w
 H = h
 
-def move
+Grids = H.lcm(W).times.map {
+  grid = H.times.map { [1] * W }
+  Bliz.each do |pos, _|
+    grid[pos[1]][pos[0]] = nil
+  end
   Bliz.each do |pos, dir|
     pos[0..] = pos + dir
     pos[0] %= W
     pos[1] %= H
   end
-end
-
-def make_grid
-  grid = H.times.map { [1] * W }
-  Bliz.each do |pos, _|
-    grid[pos[1]][pos[0]] = nil
-  end
   grid
+}
+
+def solve(minute0, start, target)
+  timeline = []
+  memo = {}
+  minute0.step do |minute|
+    ngi = (minute + 1) % Grids.size
+    ng = Grids[ngi]
+
+    nt = []
+
+    test_and_add = ->(pos) {
+      return if !ng[pos[1]][pos[0]]
+      return if memo[[pos, ngi]]
+      memo[[pos, ngi]] = true
+      nt << pos
+    }
+
+    test_and_add.(start) if (minute - minute0) <= Grids.size
+
+    timeline.each do |pos|
+      test_and_add.(pos)
+      Dirs.each do |d|
+        np = pos + d
+        return minute + 1 if np == target
+        next if np[0] < 0 || np[0] >= W
+        next if np[1] < 0 || np[1] >= H
+        test_and_add.(np)
+      end
+    end
+
+    timeline = nt
+  end
 end
 
-Grids = [make_grid]
+ab = solve(0, Vector[0, 0], Vector[W-1, H])
+p ab
+ba = solve(ab, Vector[W-1, H-1], Vector[0, -1])
+p solve(ba, Vector[0, 0], Vector[W-1, H])
 
-@cur_move = 0
-def gen()
-  @cur_move += 1
-  move
-  Grids << [make_grid]
-end
 
-bfs = []
-while true do
-end
+
+
 
 
 
