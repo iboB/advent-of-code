a5031d94524ea865a2990135175b15e2bfe00c62
Borislav Stanimirov <b.stanimirov@abv.bg>
2023-11-30 17:06:05 +0200

    2015 19 optimization ideas

--- 2015/19/day19.rb
+++ 2015/19/day19.rb
@@ -27,33 +27,50 @@ class String
 end
 
 # a
-p reps.map { target.all_reps(_1) }.flatten.uniq.length
+# p reps.map { target.all_reps(_1) }.flatten.uniq.length
 
 # # b
-# vars = Set['e']
 
-# step = 0
-# until vars === target
-#   new_vars = Set[]
-#   vars.each do |v|
-#     reps.each do |rep|
-#       v.all_reps(rep).each do |mod|
-#         new_vars << mod
-#       end
-#     end
-#   end
-#   step += 1
-#   vars = new_vars
-#   p vars.length
-# end
+# 3
+# 18
+# 105
+# 607
+# 3566
+# 21287
+# 129071
+# 793306
+# RnArRnArRnArRnYArRnArRnYArRnRnRnArArRnArYArRnArRnArRnArRnRnArYRnArRnArRnArRnArArRnArRnArRnArRnArRnArRnYArRnArRnRnArRnArRnRnArYRnArArArRnArRnArRnArRnAr
 
-# # 3
-# # 18
-# # 105
-# # 607
-# # 3566
-# # 21287
-# # 129071
-# # 793306
+# somewhat hardcoded, but I'm too lazy to write a general solution
+def ys(str) = str.scan('Y').length
+def rns(str) = str.scan('Rn').length
+def ars(str) = str.scan('Ar').length
+
+
+Ys = ys(target)
+Rns = rns(target)
+Ars = ars(target)
+
+vars = Set['e']
+
+step = 0
+until vars === target
+  new_vars = Set[]
+  vars.each do |v|
+    reps.each do |rep|
+      v.all_reps(rep).each do |mod|
+        next if ys(mod) > Ys
+        next if rns(mod) > Rns
+        next if ars(mod) > Ars
+        new_vars << mod
+      end
+    end
+  end
+  step += 1
+  vars = new_vars
+
+  p "MAX #{vars.map{ ys(_1) }.max}"
+  p vars.length
+end
 
 # p step
