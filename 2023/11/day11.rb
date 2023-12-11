da6a8bf8bd59ac9a9b566accff4922aa7acd2cf2
Borislav Stanimirov <b.stanimirov@abv.bg>
2023-12-11 14:18:41 +0200

    2023 11 clean

--- 2023/11/day11.rb
+++ 2023/11/day11.rb
@@ -1,28 +1,17 @@
-gal = []
-
-col_pad = nil
-row_pad = []
-input = File.readlines('input.txt').each.with_index { |l, y|
-  l.strip!
-  col_pad = [0] * l.length if !col_pad
-
-  row_gal = l.chars.map.with_index { |c, x|
-    if c == ?#
-      col_pad[x] += 1
-      x + y * 1i
-    end
+points = File.readlines('input.txt').flat_map.with_index { |l, y|
+  l.strip!.chars.map.with_index { |c, x|
+    [x, y] if c == ?#
   }.compact
-  row_pad << row_gal.length
-  gal += row_gal
 }
 
-s = 0
-row_pad.map! { _1 == 0 ? s += 2-1 : s}
-s = 0
-col_pad.map! { _1 == 0 ? s += 2-1 : s}
+empty = points.transpose.map { (0.._1.max).to_a - _1.tally.keys }
 
-p gal.map {
-  _1 + col[_1.real] + row[_1.imag] * 1i
-}.combination(2).map { |a, b|
-  (a.real - b.real).abs + (a.imag - b.imag).abs
-}.sum
+puts [2, 1_000_000].map { |expand|
+  add = expand - 1
+
+  points.map { |pt|
+    pt.zip(empty).map { |c, e| c + add * e.count { _1 < c } }
+  }.combination(2).map { |a, b|
+    a.zip(b).map { (_1 - _2).abs }.sum
+  }.sum
+}
