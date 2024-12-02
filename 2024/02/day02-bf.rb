8369e0bb0aade2194a6d7a208f825a517e2e27c5
Borislav Stanimirov <b@ibob.bg>
2024-12-02 22:11:27 +0200

    2024 02 style

--- 2024/02/day02-bf.rb
+++ 2024/02/day02-bf.rb
@@ -2,13 +2,13 @@
 # no point in being too clever
 
 def good(r) = r.each_cons(2).map { _1 - _2 }.all? { (1..3) === _1 }
-def good_sans_1(r) = r.each_index.any? { |i| good(r.dup.tap { _1.delete_at(i) }) }
+def good_sans_1(r) = r.combination(r.size - 1).any? { good _1 }
 
 reps = File.readlines('input.txt').map { _1.split.map(&:to_i) }
 reps += reps.map(&:reverse)
 
 # a
-p reps.count { good(_1) }
+p reps.count { good _1 }
 
 # b (no point in checking a here, since if it's good, it's also good without one element)
-p reps.count { good_sans_1(_1) }
+p reps.count { good_sans_1 _1 }
