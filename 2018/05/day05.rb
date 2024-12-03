a94ff9abe892c3a442a3dcc9dac69fea167794f7
Borislav Stanimirov <b@ibob.bg>
2024-12-03 05:13:55 +0200

    2018 05 improve

--- 2018/05/day05.rb
+++ 2018/05/day05.rb
@@ -1,25 +1,15 @@
-# brute force... takes some tens of seconds to run
-# ruby is at fault here. with a linked list this will likely be
-# 1-2 orders of magnitude faster
-
 input = File.read('input.txt').strip
 
 def react(str)
-  pd = str.bytes.each_cons(2).map { _1 - _2 }
-  while true do
-    i = pd.find_index { _1.abs == 32 }
-    break unless i
-    if i == 0
-      pd.shift(2)
-    elsif i == pd.length - 1
-      pd.pop(2)
+  build = []
+  str.bytes.each do |b|
+    if build.last == b ^ 32
+      build.pop
     else
-      d = pd.delete_at(i) + pd.delete_at(i)
-      pd[i-1] += d
+      build.push(b)
     end
   end
-
-  pd.length + 1
+  build.size
 end
 
 # a
