a9f71f0d3cc6d392ca63652f0e9cc8252d60666f
Borislav Stanimirov <b@ibob.bg>
2024-12-09 09:32:18 +0200

    2024 09 shorten a

--- 2024/09/day09a.rb
+++ 2024/09/day09a.rb
@@ -1,24 +1,18 @@
-input = File.read('input.txt').strip.chars.map(&:to_i).map.with_index {
+i = File.read('input.txt').strip.chars.map(&:to_i).map.with_index {
   [_2 & 1 == 0 ? _2/2 : nil, _1.to_i]
 }.reject { _2 == 0 }
 
 result = []
-until input.empty?
-  id, n = input.shift
-  if id
-    result += [id] * n
-    next
+until i.empty?
+  id, n = i.shift
+  result += if id
+    [id] * n
   else
-    while n != 0
-      tail = input[-1]
-      if tail[1] == 0 || tail[0].nil?
-        input.pop
-        next
-      end
-      result << tail[0]
-      tail[1] -= 1
-      n -= 1
-    end
+    n.times.map {
+      i.pop while i.last[1] == 0 || i.last[0].nil?
+      i.last[1] -= 1
+      i.last[0]
+    }
   end
 end
 
