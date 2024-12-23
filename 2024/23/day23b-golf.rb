a3d34505115783df843177fabe4ec0ed5f99d79a
Borislav Stanimirov <b@ibob.bg>
2024-12-23 11:23:44 +0200

    2024 23 even golfier golf

--- 2024/23/day23b-golf.rb
+++ 2024/23/day23b-golf.rb
@@ -1,4 +1,3 @@
-@es = File.readlines('input.txt').map { |l| l.strip.split(?-) }.then { _1 + _1.map(&:reverse) }.to_set
-@ns = @es.to_a.flatten.sort.uniq
-def max_clique(cliq = []) = @ns[(@ns.index(cliq.last) || -1) + 1..].map { |n| max_clique(cliq + [n]) if cliq.all? { @es === [_1, n] } }.compact.max_by(&:size) || cliq
-puts max_clique.join(?,)
+N,E=File.readlines('input.txt').map{_1.strip.split(?-)}.then{[_1.flatten.sort.uniq,(_1+_1.map(&:reverse)).to_set]}
+def mc(c=[])=N[(N.index(c[-1])||-1)+1..].map{|n|mc(c+[n])if c.all?{E===[_1,n]}}.compact.max_by(&:size)||c
+puts mc*?,
