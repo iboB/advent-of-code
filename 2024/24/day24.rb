a31b8855b5424e103c266ce6f2457f684aa75769
Borislav Stanimirov <b@ibob.bg>
2024-12-24 17:45:17 +0200

    2024 24 (not really b)

--- 2024/24/day24.rb
+++ 2024/24/day24.rb
@@ -26,16 +26,61 @@ init, prog = File.read('input.txt').split("\n\n").then { |init, prog|
   [init, prog]
 }
 
-p num(calc(init, prog), ?z)
+base = calc(init, prog)
+z = num(base, ?z)
+p z # a
 
-# x =  num('x')
-# y =  num('y')
-# p [x, y]
-# p (x).to_s(2)
-# p (y).to_s(2)
-# p (x+y).to_s(2)
-# p num('z').to_s(2)
+### then b was solved mostly on pen and paper with experiments with
+### "adding" differrent numbers and finding the mistakes
 
-# p '     ' + (num('z')^(x+y)).to_s(2)
+x = num(base, ?x)
+y = num(base, ?y)
 
-# (num('z')^(x+y)).to_s(2).chars.count(?1).display
+zs = base.keys.select { _1[0] == ?z }.sort
+
+target_z = x + y
+bad = (z ^ target_z).to_s(2).chars.map.with_index { |c, i| c == ?1 ? zs[i] : nil }.compact
+good = zs - bad
+
+
+def set_xy(x, y)
+  xs, ys = 100.times.map { |i|
+    x, xb = x.divmod(2)
+    y, yb = y.divmod(2)
+    is = "%02d" % i
+    [[is, xb], [is, yb]]
+  }.transpose
+  xs.map { [?x + _1, _2] } + ys.map { [?y + _1, _2] }
+end
+
+a = calc(set_xy(2**3, 2**2).to_h, prog)
+p num(a, ?z)
+
+exit
+
+prog = prog.map { |x, op, y, z| [z, [op, x, y]] }.to_h
+
+def find_deps(prog, keys)
+  deps = keys.to_set
+  dfs = keys.dup
+  until dfs.empty?
+    k = dfs.pop
+    op, x, y = prog[k]
+
+    if !deps.include?(x) && prog[x]
+      dfs << x
+      deps << x
+    end
+
+    if !deps.include?(y) && prog[y]
+      dfs << y
+      deps << y
+    end
+  end
+  deps
+end
+
+good_deps = find_deps(prog, good)
+
+p bad
+p bad.map { find_deps(prog, [_1]) }[..2]
