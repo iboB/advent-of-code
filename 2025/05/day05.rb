f00f11dde466b3d9eda7f288b65bd17048e967be
Borislav Stanimirov <b@ibob.bg>
2025-12-05 08:18:31 +0200

    2025 05

---
+++ 2025/05/day05.rb
@@ -0,0 +1,18 @@
+rs, ids = File.read(?i).strip.split("\n\n").then {[
+  _1.lines.map{|l| l.split(?-).map(&:to_i)},
+  _2.lines.map(&:to_i)
+]}
+
+# a
+p ids.select { |i| rs.any? { (_1.._2) === i } }.size
+
+# b
+# sweep line:
+# store depth in d, collect starts with depth 0->1 and ends with depth 1->0
+d = 0
+p rs.flat_map { [[_1, 1], [_2 + 1, -1]] }.sort.select {
+  d += _2
+  d == 1 && _2 == 1 || d == 0 && _2 == -1
+}.each_slice(2).map {
+  _2[0] - _1[0]
+}.sum
