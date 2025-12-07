5e77f28b1f1053713b2913231705841a90cf3f8c
Borislav Stanimirov <b@ibob.bg>
2025-12-07 15:42:54 +0200

    2025 07 minor tweak

--- 2025/07/day07.rb
+++ 2025/07/day07.rb
@@ -2,13 +2,12 @@ cur, *grid = File.readlines(?i).map { |l| l.strip.chars.map { _1 == ?. ? 0 : 1 }
 
 nsplits = 0
 last = grid.inject(cur) { |cur, row|
-  cur.zip(row).each.with_index { |(c, r), i|
+  row.each_with_index { |r, i|
+    c = cur[i]
     next if c * r == 0
     nsplits += 1
-
-    n = cur[i]
-    cur[i - 1] += n
-    cur[i + 1] += n
+    cur[i - 1] += c
+    cur[i + 1] += c
     cur[i] = 0
   }
   cur
