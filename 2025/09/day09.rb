24c43704ddba311c1553b91bee24e06a7cd7af3f
Borislav Stanimirov <b@ibob.bg>
2025-12-09 12:18:33 +0200

    2025 09 minor touches

--- 2025/09/day09.rb
+++ 2025/09/day09.rb
@@ -1,21 +1,16 @@
-input = File.readlines(?i).map { |l|
-  l.split(?,).map(&:to_i)
-}
+input = File.readlines(?i).map { |l| l.split(?,).map(&:to_i) }
 
 # find border (and sort each segment's coords)
 
 border = (input + input[...1]).each_cons(2).map {
-  [_1, _2].transpose.map(&:sort).transpose
+  _1.transpose.map(&:sort).flatten
 }
 
 # for each candidate check if it intersects with border
 # if yes, negative area (part A), else positive area (part B)
 
-p input.combination(2).map {
-  (ax, bx), (ay, by) = [_1, _2].transpose.map(&:sort)
-  sign = border.find { |(bax, bay), (bbx, bby)|
-    bbx > ax && bax < bx && bby > ay && bay < by
-  } ? -1 : 1
+p input.combination(2).map { |co|
+  (ax, bx), (ay, by) = co.transpose.map(&:sort)
+  sign = border.any? { _1 < bx && _2 > ax && _3 < by && _4 > ay } ? -1 : 1
   sign * (bx - ax + 1) * (by - ay + 1)
 }.minmax.map(&:abs)
-
